05154057591cf5a792591aa1fbacb52cf2d07084
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-10 14:08:46 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static PizzaOrderingSystem.Common.ModelValidationConstants;
+
+namespace PizzaOrderingSystem.Web.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(ConfirmPassword))]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = UserValidation.UsernameRequired)]
+        [MinLength(UserValidation.UsernameMinLength, ErrorMessage = UserValidation.UserNameMinLengthError)]
+        [MaxLength(UserValidation.UsernameMaxLength, ErrorMessage = UserValidation.UserNameMaxLengthError)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = UserValidation.FirstNameRequired)]
+        [MinLength(UserValidation.FirstNameMinLength, ErrorMessage = UserValidation.FirstNameMinLengthError)]
+        [MaxLength(UserValidation.FirstNameMaxLength, ErrorMessage = UserValidation.FirstNameMaxLengthError)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = UserValidation.LastNameRequired)]
+        [MinLength(UserValidation.LastNameMinLength, ErrorMessage = UserValidation.LastNameMinLengthError)]
+        [MaxLength(UserValidation.LastNameMaxLength, ErrorMessage = UserValidation.LastNameMaxLengthError)]
+        public string LastName { get; set; }
+
+    }
+}
