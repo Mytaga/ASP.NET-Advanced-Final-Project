335f501466b518d506a0f2eee80bc3627747a7ba
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-10 15:56:31 +0300

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/RegisterViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using static PizzaOrderingSystem.Common.ModelValidationConstants;
 
 namespace PizzaOrderingSystem.Web.ViewModels.Account
@@ -11,9 +12,11 @@ namespace PizzaOrderingSystem.Web.ViewModels.Account
 
         [Required]
         [Compare(nameof(ConfirmPassword))]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = UserValidation.UsernameRequired)]
