e83ac51186147e02e02d83c52de46caba6327f78
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-13 00:07:59 +0300

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/BaseController.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/BaseController.cs
@@ -1,7 +1,9 @@
 ﻿namespace PizzaOrderingSystem.Web.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    [Authorize]
     public class BaseController : Controller
     {
     }
--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/HomeController.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/HomeController.cs
@@ -5,19 +5,23 @@
     using PizzaOrderingSystem.Web.ViewModels;
 
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
 
     public class HomeController : BaseController
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return this.View();
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return this.View();
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
