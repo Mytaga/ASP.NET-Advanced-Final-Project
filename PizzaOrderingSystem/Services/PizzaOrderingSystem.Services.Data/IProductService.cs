991895043f16fe5e6f1fcb80a553c286bd11f274
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-13 17:10:57 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public interface IProductService
+    {
+        IQueryable<Product> GetAllByName(string searchName = "");
+
+        IQueryable<Product> GetAllByCategory(string categoryName = "");
+
+        ICollection<string> GetAllFurnitureCategories();
+
+        Task<Product> GetByIdАsync(string id);
+
+        Task AddProduct(Product product);
+
+        void DeleteProduct(Product product);
+
+        void EditProduct(Product product);
+
+        bool ExistById(string id);
+    }
+}
---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using PizzaOrderingSystem.Data.Common.Repositories;
+using PizzaOrderingSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public class ProductService : IProductService
+    {
+        private const string EmptyString = "";
+        private readonly IDeletableEntityRepository<Product> productRepo;
+
+        public ProductService(IDeletableEntityRepository<Product> productRepo)
+        {
+            this.productRepo = productRepo;
+        }
+
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> GetAllByCategory(string categoryName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> GetAllByName(string searchName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<string> GetAllFurnitureCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdАsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
