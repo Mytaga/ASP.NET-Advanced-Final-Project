40824a44f889f3a0e73341071be2ea0d1fb6dc30
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-13 17:08:44 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Services.Data;
+
+namespace PizzaOrderingSystem.Web.Controllers
+{
+    public class ProductController : BaseController
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
