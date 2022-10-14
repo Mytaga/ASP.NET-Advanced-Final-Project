b9850beb73ff2b91a9f40f858ae581acd322d5f2
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-14 17:15:49 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Common;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Data;
+using PizzaOrderingSystem.Services.Mapping;
+using PizzaOrderingSystem.Web.ViewModels.CategoryViewModels;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Web.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Create()
+        {
+            CreateCategoryViewModel viewModel = new CreateCategoryViewModel();
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public async Task<IActionResult> Create(CreateCategoryInputModel model)
+        {
+            var category = AutoMapperConfig.MapperInstance.Map<Category>(model);
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction("Create", "Category");
+            }
+
+            if (this.categoryService.ExistById(category.Id))
+            {
+                return this.RedirectToAction("Create", "Category");
+            }
+
+            await this.categoryService.AddCategory(category);
+
+            return this.RedirectToAction("Index", "Product");
+        }
+    }
+}
--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ProductController.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ProductController.cs
@@ -1,22 +1,77 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Common;
+using PizzaOrderingSystem.Data.Models;
 using PizzaOrderingSystem.Services.Data;
+using PizzaOrderingSystem.Services.Mapping;
+using PizzaOrderingSystem.Web.ViewModels.ProductViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PizzaOrderingSystem.Web.Controllers
 {
     public class ProductController : BaseController
     {
         private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, ICategoryService categoryService)
         {
             this.productService = productService;
+            this.categoryService = categoryService;
         }
 
+        [HttpGet]
         [AllowAnonymous]
-        public IActionResult Index()
+        public IActionResult Index(string search)
         {
-            return this.View();
+            IQueryable<Product> allProducts = this.productService.GetAllByName(search);
+
+            AllProductsViewModel viewModel = new AllProductsViewModel()
+            {
+                Products = allProducts.To<ListAllProductsViewModel>().ToArray(),
+                SearchQuery = search,
+            };
+
+            return this.View(viewModel);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Create()
+        {
+            ICollection<ListProductCategoriesViewModel> allCategories =
+                this.categoryService.All()
+                .To<ListProductCategoriesViewModel>()
+                .ToArray();
+
+            CreateProductViewModel viewModel = new CreateProductViewModel();
+
+            viewModel.Categories = allCategories;
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public async Task<IActionResult> Create(CreateProductInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction("Create", "Product");
+            }
+
+            if (!this.categoryService.ExistById(model.CategoryId))
+            {
+                return this.RedirectToAction("Create", "Product");
+            }
+
+            Product product = AutoMapperConfig.MapperInstance.Map<Product>(model);
+
+            await this.productService.AddProduct(product);
+
+            return this.RedirectToAction("Index", "Product");
         }
     }
 }
