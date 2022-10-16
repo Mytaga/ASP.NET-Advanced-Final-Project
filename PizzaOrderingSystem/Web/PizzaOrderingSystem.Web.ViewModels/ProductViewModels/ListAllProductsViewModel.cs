c078f2c311e7265f1e14552daeac063ea9b719b1
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-16 21:11:22 +0300

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ProductViewModels/ListAllProductsViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ProductViewModels/ListAllProductsViewModel.cs
@@ -12,5 +12,7 @@ namespace PizzaOrderingSystem.Web.ViewModels.ProductViewModels
         public decimal Price { get; set; }
 
         public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
     }
 }
