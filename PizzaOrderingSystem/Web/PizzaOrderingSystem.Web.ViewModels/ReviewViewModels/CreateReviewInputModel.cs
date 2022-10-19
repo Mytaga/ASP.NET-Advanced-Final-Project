38ad3f1454090ccdc6585f32c9dcdc30b80e7bfe
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-19 18:01:03 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ReviewViewModels/CreateReviewInputModel.cs
@@ -0,0 +1,25 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Mapping;
+using System;
+using System.ComponentModel.DataAnnotations;
+using static PizzaOrderingSystem.Common.ModelValidationConstants;
+
+namespace PizzaOrderingSystem.Web.ViewModels.ReviewViewModels
+{
+    public class CreateReviewInputModel : IMapTo<Review>
+    {
+        [Required(ErrorMessage = ReviewValidation.NameRequiredError)]
+        [MinLength(ReviewValidation.AuthorNameMinLength, ErrorMessage = ReviewValidation.NameMinLengthError)]
+        [MaxLength(ReviewValidation.AuthorNameMaxLength, ErrorMessage = ReviewValidation.NameMaxLengthError)]
+        public string AuthorName { get; set; }
+
+        [Required(ErrorMessage = ReviewValidation.ContentRequiredError)]
+        [MinLength(ReviewValidation.ContentMinLength, ErrorMessage = ReviewValidation.ContentMinLengthError)]
+        [MaxLength(ReviewValidation.ContentMaxLength, ErrorMessage = ReviewValidation.ContentMaxLengthError)]
+        public string Content { get; set; }
+
+        public DateTime PublishedOn => DateTime.UtcNow;
+
+        public string UserId { get; set; }
+    }
+}
---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ReviewViewModels/ReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Mapping;
+using System;
+
+namespace PizzaOrderingSystem.Web.ViewModels.ReviewViewModels
+{
+    public class ReviewViewModel : IMapFrom<Review>
+    {
+        public string AuthorName { get; set; }
+
+        public string Content { get; set; }
+
+        public string PublishedOn { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
