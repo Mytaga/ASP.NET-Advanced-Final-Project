7819af0a7df65058fef9f7e354a89da1404d4b71
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-19 17:55:30 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IReviewService.cs
@@ -0,0 +1,17 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public interface IReviewService
+    {
+        IQueryable<Review> GetAll();
+
+        Task AddReview(Review review);
+
+        Task DeleteReview(Review review);
+
+        Task EditReview(Review review);
+    }
+}
---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/ReviewService.cs
@@ -0,0 +1,41 @@
+﻿using PizzaOrderingSystem.Data.Common.Repositories;
+using PizzaOrderingSystem.Data.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IDeletableEntityRepository<Review> reviewRepo;
+
+        public ReviewService(IDeletableEntityRepository<Review> reviewRepo)
+        {
+            this.reviewRepo = reviewRepo;
+        }
+
+        public async Task AddReview(Review review)
+        {
+            await this.reviewRepo.AddAsync(review);
+            await this.reviewRepo.SaveChangesAsync();
+        }
+
+        public async Task DeleteReview(Review review)
+        {
+            this.reviewRepo.Delete(review);
+            await this.reviewRepo.SaveChangesAsync();
+        }
+
+        public async Task EditReview(Review review)
+        {
+            this.reviewRepo.Update(review);
+            await this.reviewRepo.SaveChangesAsync();
+        }
+
+        public IQueryable<Review> GetAll()
+        {
+            return this.reviewRepo.All();
+        }
+    }
+}
