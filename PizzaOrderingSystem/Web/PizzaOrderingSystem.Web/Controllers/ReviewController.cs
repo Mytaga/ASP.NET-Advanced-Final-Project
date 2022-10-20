d3441e07da76d74701f0a43d46d675accc9e8129
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-20 11:01:54 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ReviewController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Common;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Data;
+using PizzaOrderingSystem.Services.Mapping;
+using PizzaOrderingSystem.Web.ViewModels.ReviewViewModels;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Web.Controllers
+{
+    public class ReviewController : BaseController
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            IQueryable<Review> allReviews = this.reviewService.GetAll();
+
+            AllReviewsViewModel viewModel = new AllReviewsViewModel()
+            {
+                Reviews = allReviews.To<ReviewViewModel>().ToList(),
+            };
+
+            return this.View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            CreateReviewViewModel viewModel = new CreateReviewViewModel();
+
+            viewModel.AuthorName = this.User.Identity.Name;
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateReviewInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction(nameof(this.Create));
+            }
+
+            Review review = AutoMapperConfig.MapperInstance.Map<Review>(model);
+            review.UserId = this.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            await this.reviewService.AddReview(review);
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Delete(string id)
+        {
+            var product = this.reviewService.GetById(id);
+
+            if (product == null)
+            {
+                return this.RedirectToAction("Error", "Home");
+            }
+
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var product = await this.reviewService.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return this.RedirectToAction("Error", "Home");
+            }
+
+            await this.reviewService.DeleteReview(product);
+
+            return this.RedirectToAction("Index", "Review");
+        }
+    }
+}
