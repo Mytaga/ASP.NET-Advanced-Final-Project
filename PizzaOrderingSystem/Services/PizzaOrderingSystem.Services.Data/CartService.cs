f20ad7dde1ef4a96b1153fdc4dbfdcec6ec70b9f
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-21 16:06:54 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/CartService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using PizzaOrderingSystem.Data;
+using PizzaOrderingSystem.Data.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public class CartService : ICartService
+    {
+        public Task AddToCartAsync(Product product, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFromCartAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public interface ICartService
+    {
+        Task AddToCartAsync(Product product, int quantity);
+
+        Task RemoveFromCartAsync(Product product);
+    }
+}
