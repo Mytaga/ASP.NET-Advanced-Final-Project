7d51db9e26f377cfb990e25f5ed118a7bae0c56b
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-21 16:09:25 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaOrderingSystem.Web.Controllers
+{
+    public class ShoppingCartController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
