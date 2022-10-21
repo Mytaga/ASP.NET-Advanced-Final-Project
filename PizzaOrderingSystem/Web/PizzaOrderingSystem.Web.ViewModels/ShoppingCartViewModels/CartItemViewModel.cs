acb46ddc9dc4629ef4dfaa4b0564bd81dce3ff20
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-21 16:07:57 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ShoppingCartViewModels/CartItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Mapping;
+
+namespace PizzaOrderingSystem.Web.ViewModels.ShoppingCart
+{
+    public class CartItemViewModel : IMapFrom<CartItem>
+    {
+        public string ItemName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal ItemPrice { get; set; }
+
+        public decimal Amount => this.ItemPrice * this.Quantity;
+    }
+}
---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ShoppingCartViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace PizzaOrderingSystem.Web.ViewModels.ShoppingCart
+{
+    public class ShoppingCartViewModel
+    {
+        public ICollection<CartItemViewModel> CartItems { get; set; }
+
+        public decimal Total => this.CartItems.Sum(ci => ci.Amount);
+
+        public string ExpectedDelivery => DateTime.Now.AddHours(1).ToString("f", CultureInfo.InvariantCulture);
+    }
+}
