8950653e15a5f204c264c751595b7a4c1ea2fa95
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-23 21:09:21 +0300

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ShoppingCartController.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/ShoppingCartController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Data;
+using PizzaOrderingSystem.Services.Mapping;
+using PizzaOrderingSystem.Web.ViewModels.ShoppingCart;
+using System.Linq;
 
 namespace PizzaOrderingSystem.Web.Controllers
 {
     public class ShoppingCartController : BaseController
     {
+        private readonly ICartItemService cartItemService;
+        private readonly ICartService cartService;
+
+        public ShoppingCartController(ICartItemService cartItemService, ICartService cartService)
+        {
+            this.cartItemService = cartItemService;
+            this.cartService = cartService;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return this.View();
+            IQueryable<CartItem> allItems = this.cartItemService.GetAllByName();
+
+            ShoppingCartViewModel viewModel = new ShoppingCartViewModel()
+            {
+                CartItems = allItems.To<CartItemViewModel>().ToArray(),
+            };
+
+            return this.View(viewModel);
         }
     }
 }
