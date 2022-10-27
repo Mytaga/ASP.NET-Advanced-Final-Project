74d2231ad2d1de07fa98ca6b5b8a3e1da1c11153
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-27 16:25:43 +0300

    Add files via upload

---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/AddressService.cs
@@ -0,0 +1,22 @@
+﻿using PizzaOrderingSystem.Data.Common.Repositories;
+using PizzaOrderingSystem.Data.Models;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public class AddressService : IAddressService
+    {
+        private IDeletableEntityRepository<Address> addressRepo;
+
+        public AddressService(IDeletableEntityRepository<Address> addressRepo)
+        {
+            this.addressRepo = addressRepo;
+        }
+
+        public async Task AddAddress(Address address)
+        {
+            await this.addressRepo.AddAsync(address);
+            await this.addressRepo.SaveChangesAsync();
+        }
+    }
+}
---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IAddressService.cs
@@ -0,0 +1,14 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public interface IAddressService
+    {
+        Task AddAddress(Address address);
+    }
+}
