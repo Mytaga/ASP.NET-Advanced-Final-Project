5ed2b1650308eb6005f82852b3c641423f7e5dec
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-29 19:45:02 +0300

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/ProfileViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/ProfileViewModel.cs
@@ -1,9 +1,11 @@
-﻿using PizzaOrderingSystem.Data.Models;
+﻿using AutoMapper;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Mapping;
 using System.Collections.Generic;
 
 namespace PizzaOrderingSystem.Web.ViewModels.Account
 {
-    public class ProfileViewModel
+    public class ProfileViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
         public string UserName { get; set; }
 
@@ -14,6 +16,8 @@ namespace PizzaOrderingSystem.Web.ViewModels.Account
         public string LastName { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public string FullName => this.FirstName + " " + this.LastName;
 
         public string ImageUrl { get; set; }
 
@@ -30,5 +34,19 @@ namespace PizzaOrderingSystem.Web.ViewModels.Account
         public virtual CreditCard CreditCard { get; set; }
 
         public virtual ICollection<Order> Orders => new HashSet<Order>();
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ApplicationUser, ProfileViewModel>()
+                 .ForMember(d => d.City, mo => mo.MapFrom(s => s.Address.City));
+            configuration.CreateMap<ApplicationUser, ProfileViewModel>()
+                 .ForMember(d => d.Street, mo => mo.MapFrom(s => s.Address.Street));
+            configuration.CreateMap<ApplicationUser, ProfileViewModel>()
+                 .ForMember(d => d.StreetNumber, mo => mo.MapFrom(s => s.Address.StreetNumber));
+            configuration.CreateMap<ApplicationUser, ProfileViewModel>()
+                 .ForMember(d => d.Floor, mo => mo.MapFrom(s => s.Address.Floor));
+            configuration.CreateMap<ApplicationUser, ProfileViewModel>()
+                 .ForMember(d => d.PostCode, mo => mo.MapFrom(s => s.Address.PostCode));
+        }
     }
 }
--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/UpdateProfileViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/UpdateProfileViewModel.cs
@@ -1,32 +1,37 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using PizzaOrderingSystem.Common;
-using PizzaOrderingSystem.Data.Models;
-using PizzaOrderingSystem.Services.Mapping;
 using System.ComponentModel.DataAnnotations;
 
 namespace PizzaOrderingSystem.Web.ViewModels.Account
 {
-    public class UpdateProfileViewModel 
+    public class UpdateProfileViewModel
     {
-        [Phone]
         [MinLength(ModelValidationConstants.UserValidation.PhoneNumberMinLength, ErrorMessage = ModelValidationConstants.UserValidation.PhoneNumberMinLengthError)]
         [MaxLength(ModelValidationConstants.UserValidation.PhoneNumberMaxLength, ErrorMessage = ModelValidationConstants.UserValidation.PhoneNumberMaxLengthError)]
+        [Phone]
         public string PhoneNumber { get; set; }
 
         [Display(Name = "Image")]
         public IFormFile ImageUrl { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ModelValidationConstants.AddressValidation.CityRequiredError)]
+        [MinLength(ModelValidationConstants.AddressValidation.CityMinLength, ErrorMessage = ModelValidationConstants.AddressValidation.CityMinLengthError)]
+        [MaxLength(ModelValidationConstants.AddressValidation.CityMaxLength, ErrorMessage = ModelValidationConstants.AddressValidation.CityMaxLengthError)]
         public string City { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = ModelValidationConstants.AddressValidation.StreetRequiredError)]
+        [MinLength(ModelValidationConstants.AddressValidation.StreetMinLength, ErrorMessage = ModelValidationConstants.AddressValidation.StreetMinLengthError)]
+        [MaxLength(ModelValidationConstants.AddressValidation.StreetMaxLength, ErrorMessage = ModelValidationConstants.AddressValidation.StreetMaxLengthError)]
         public string Street { get; set; }
 
+        [Range(ModelValidationConstants.AddressValidation.StreetNumberMinValue, ModelValidationConstants.AddressValidation.StreetNumberMaxValue, ErrorMessage = ModelValidationConstants.AddressValidation.StreetNumberRangeLengthError)]
         public int StreetNumber { get; set; }
 
+        [Range(ModelValidationConstants.AddressValidation.FloorMinValue, ModelValidationConstants.AddressValidation.FloorMaxValue, ErrorMessage = ModelValidationConstants.AddressValidation.FloorRangeLengthError)]
         public int Floor { get; set; }
 
+        [MinLength(ModelValidationConstants.AddressValidation.PostalCodeMinLength, ErrorMessage = ModelValidationConstants.AddressValidation.PostalCodeMinLengthError)]
+        [MaxLength(ModelValidationConstants.AddressValidation.PostalCodeMaxLength, ErrorMessage = ModelValidationConstants.AddressValidation.PostalCodeMaxLengthError)]
         public string PostCode { get; set; }
     }
 }
