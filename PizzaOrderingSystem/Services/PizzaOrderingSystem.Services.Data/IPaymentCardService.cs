023098d39ab3c0ec4ff8749138473c7fb28968ee
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-31 23:28:58 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IPaymentCardService.cs
@@ -0,0 +1,16 @@
+﻿using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Web.ViewModels.PaymentCardViewModels;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public interface IPaymentCardService
+    {
+        Task Add(AddCardViewModel viewModel);
+
+        Task<AllCardsViewModel> GetAll();
+
+        Task Delete(CreditCard card);
+    }
+}
---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/PaymentCardService.cs
@@ -0,0 +1,33 @@
+﻿using PizzaOrderingSystem.Data.Common.Repositories;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Web.ViewModels.PaymentCardViewModels;
+using System;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public class PaymentCardService : IPaymentCardService
+    {
+        private readonly IDeletableEntityRepository<CreditCard> creditCardRepo;
+
+        public PaymentCardService(IDeletableEntityRepository<CreditCard> creditCardRepo)
+        {
+            this.creditCardRepo = creditCardRepo;
+        }
+
+        public async Task Add(AddCardViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(CreditCard card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AllCardsViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
