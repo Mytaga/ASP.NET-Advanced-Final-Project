2d602bd710360ca743d7153aadb158ba8b066f26
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-31 23:28:32 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/PaymentCardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Services.Data;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Web.Controllers
+{
+    public class PaymentCardController : Controller
+    {
+        private readonly IPaymentCardService paymentCardService;
+
+        public PaymentCardController(IPaymentCardService paymentCardService)
+        {
+            this.paymentCardService = paymentCardService;
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            return this.View();
+        }
+    }
+}
