148c980bff0161de77d89898922f49ed5d97e144
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-01 23:51:50 +0200

    Delete AllCardsViewModel.cs

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/PaymentCardViewModels/AllCardsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace PizzaOrderingSystem.Web.ViewModels.PaymentCardViewModels
-{
-    public class AllCardsViewModel
-    {
-        public virtual ICollection<PaymentCardViewModel> Cards { get; set; }
-    }
-}
