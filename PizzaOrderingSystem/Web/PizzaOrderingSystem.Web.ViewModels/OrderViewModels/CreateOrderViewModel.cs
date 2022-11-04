86cbc4b02b6722d7160cb69ec360b155ae39fe7c
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-04 15:39:46 +0200

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/OrderViewModels/CreateOrderViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/OrderViewModels/CreateOrderViewModel.cs
@@ -2,6 +2,8 @@
 using PizzaOrderingSystem.Data.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace PizzaOrderingSystem.Web.ViewModels.OrderViewModels
 {
@@ -9,23 +11,31 @@ namespace PizzaOrderingSystem.Web.ViewModels.OrderViewModels
     {
         public DateTime TimeOfOrder => DateTime.Now;
 
-        public decimal TotalPrice { get; set; }
+        [Required]
+        public string TotalPrice { get; set; }
 
         public OrderType DeliveryType => OrderType.Delivery;
 
         public OrderStatus Status => OrderStatus.Active;
 
+        public PaymentType PaymentType { get; set; }
+
+        [Required]
+        public string ExpectedDelivery => DateTime.Now.AddHours(1).ToString("f", CultureInfo.InvariantCulture);
+
+        [Required]
         public string City { get; set; }
 
+        [Required]
         public string Street { get; set; }
 
+        [Required]
         public int StreetNumber { get; set; }
 
         public int Floor { get; set; }
 
+        [Required]
         public string UserId { get; set; }
-
-        public string ShopId { get; set; }
 
         public virtual ICollection<CreditCard> Cards { get; set; }
     }
