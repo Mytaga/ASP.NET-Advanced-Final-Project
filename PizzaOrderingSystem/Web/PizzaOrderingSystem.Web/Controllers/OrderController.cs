efc4c9d92070e613c9ec9a7d1a9300127169c962
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-04 09:43:00 +0200

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/OrderController.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Controllers/OrderController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Services.Data;
+using PizzaOrderingSystem.Web.ViewModels.OrderViewModels;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace PizzaOrderingSystem.Web.Controllers
 {
     public class OrderController : BaseController
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ICartService cartService;
+
+        public OrderController(UserManager<ApplicationUser> userManager, ICartService cartService)
+        {
+            this.userManager = userManager;
+            this.cartService = cartService;
+        }
+
         public IActionResult Index()
         {
             return this.View();
+        }
+
+        public async Task<IActionResult> Confirm()
+        {
+            var userId = this.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+ 
+            var user = await this.userManager.FindByIdAsync(userId);
+
+            CreateOrderViewModel viewModel = new CreateOrderViewModel()
+            {
+                TotalPrice = this.cartService.GetShoppingCartTotal(),
+                UserId = userId,
+                Cards = user.CreditCards,
+                City = user.Address.City,
+                Street = user.Address.Street,
+                StreetNumber = user.Address.StreetNumber,
+                Floor = user.Address.Floor,
+                ShopId = null,
+            };
+
+            return this.View(viewModel);
         }
     }
 }
