1c02c8531af44b16922877c35d29e042094e18dc
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-04 15:38:33 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using PizzaOrderingSystem.Web.ViewModels.OrderViewModels;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public interface IOrderService
+    {
+        Task AddAsync(CreateOrderViewModel viewModel);
+    }
+}
---
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using PizzaOrderingSystem.Data.Common.Repositories;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Web.ViewModels.OrderViewModels;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IDeletableEntityRepository<Order> orderRepo;
+
+        public OrderService(IDeletableEntityRepository<Order> orderRepo)
+        {
+            this.orderRepo = orderRepo;
+        }
+
+        public async Task AddAsync(CreateOrderViewModel viewModel)
+        {
+            Order order = new Order()
+            {
+                TimeOfOrder = viewModel.TimeOfOrder,
+                TotalPrice = decimal.Parse(viewModel.TotalPrice),
+                DeliveryType = viewModel.DeliveryType,
+                Status = viewModel.Status,
+                PaymentType = viewModel.PaymentType,
+                UserId = viewModel.UserId,
+            };
+
+            await this.orderRepo.AddAsync(order);
+            await this.orderRepo.SaveChangesAsync();
+        }
+    }
+}
