16c38553fa600467f7ac745846dd57b7cc2c2cd5
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-08 14:24:03 +0200

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/OrderViewModels/OrderViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/OrderViewModels/OrderViewModel.cs
@@ -1,27 +1,18 @@
 ﻿using PizzaOrderingSystem.Data.Models;
-using PizzaOrderingSystem.Data.Models.Enums;
 using PizzaOrderingSystem.Services.Mapping;
-using System;
-using System.Collections.Generic;
 
 namespace PizzaOrderingSystem.Web.ViewModels.OrderViewModels
 {
     public class OrderViewModel : IMapFrom<Order>
     {
-        public DateTime TimeOfOrder { get; set; }
+        public string TimeOfOrder { get; set; }
 
-        public decimal TotalPrice { get; set; }
+        public string TotalPrice { get; set; }
 
-        public OrderType DeliveryType { get; set; }
+        public string DeliveryType { get; set; }
 
-        public OrderStatus Status { get; set; }
+        public string OrderId { get; set; }
 
-        public string UserId { get; set; }
-
-        public string ShopId { get; set; }
-
-        public string City { get; set; }
-
-        public ICollection<Product> Products { get; set; }
+        public string Status { get; set; }
     }
 }
