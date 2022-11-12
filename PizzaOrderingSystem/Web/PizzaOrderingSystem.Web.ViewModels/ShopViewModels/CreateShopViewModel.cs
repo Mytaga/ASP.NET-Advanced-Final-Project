5bbf63ea94cecb420c7ab2d3d58c9df6d505b181
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-12 16:57:36 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ShopViewModels/CreateShopViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PizzaOrderingSystem.Web.ViewModels.ShopViewModels
+{
+    public class CreateShopViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public int StreetNumber { get; set; }
+    }
+}
---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ShopViewModels/ShopViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PizzaOrderingSystem.Common;
+
+namespace PizzaOrderingSystem.Web.ViewModels.ShopViewModels
+{
+    public class ShopViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public int StreetNumber { get; set; }
+
+        public string ImageUrl => GlobalConstants.ShopImageUrl;
+    }
+}
