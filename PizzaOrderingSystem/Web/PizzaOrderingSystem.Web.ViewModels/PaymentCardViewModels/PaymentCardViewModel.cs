0a0160c2ec1957d1a8d137d28aa5c65a3a4cda60
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-14 22:46:46 +0200

    Add files via upload

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/PaymentCardViewModels/PaymentCardViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/PaymentCardViewModels/PaymentCardViewModel.cs
@@ -5,6 +5,8 @@ namespace PizzaOrderingSystem.Web.ViewModels.PaymentCardViewModels
 {
     public class PaymentCardViewModel : IMapFrom<CreditCard>
     {
+        public string Id { get; set; }
+
         public string CardNumber { get; set; }
 
         public string ImageUrl => "https://img.icons8.com/color/48/000000/mastercard-logo.png";
