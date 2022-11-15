6471494b34a1dd9d3ae78ed3d00c68f69fae2283
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-15 14:37:12 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Areas/Manager/Controllers/SaleController.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaOrderingSystem.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SaleController : ManagerController
+    {
+        public SaleController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
