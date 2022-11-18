711466580effc7a065451032ae9a9a1697113729
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-18 15:58:52 +0200

    Add files via upload

--- PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/ISaleService.cs
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/ISaleService.cs
@@ -1,7 +1,12 @@
-﻿namespace PizzaOrderingSystem.Services.Data
+﻿using PizzaOrderingSystem.Web.ViewModels.SaleViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Services.Data
 {
     public interface ISaleService
     {
-
+        Task<IEnumerable<SaleViewModel>> GetAllSalesAsync();
+        Task AddSaleAsync(CreateSaleViewModel viewModel);
     }
 }
--- PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/SaleService.cs
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/SaleService.cs
@@ -1,5 +1,11 @@
-﻿using PizzaOrderingSystem.Data.Common.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaOrderingSystem.Data.Common.Repositories;
 using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Web.ViewModels.SaleViewModels;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PizzaOrderingSystem.Services.Data
 {
@@ -10,6 +16,26 @@ namespace PizzaOrderingSystem.Services.Data
         public SaleService(IDeletableEntityRepository<Sale> saleRepo)
         {
             this.saleRepo = saleRepo;
+        }
+
+        public Task AddSaleAsync(CreateSaleViewModel viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<SaleViewModel>> GetAllSalesAsync()
+        {
+            return await this.saleRepo
+                .AllAsNoTracking()
+                .Select(s => new SaleViewModel
+                {
+                    Id = s.Id,
+                    Amount = s.Amount.ToString("C"),
+                    SaleDate = s.SaleDate,
+                    PaymentType = s.PaymentType.ToString(),
+
+                })
+                .ToListAsync();
         }
     }
 }
