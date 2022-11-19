44e47a544fa635db1a999797406ac3014896aea2
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-19 22:10:09 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/SaleViewModels/SaleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PizzaOrderingSystem.Web.ViewModels.SaleViewModels
+{
+    public class SaleViewModel
+    {
+        public string Id { get; set; }
+
+        public string Amount { get; set; }
+
+        public DateTime SaleDate { get; set; }
+
+        public string PaymentType { get; set; }
+    }
+}
