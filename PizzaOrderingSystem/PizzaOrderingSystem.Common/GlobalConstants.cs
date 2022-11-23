11cc478ce737ae4c11b126f59c307cf12ec050ed
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-23 13:53:22 +0200

    Add files via upload

--- PizzaOrderingSystem/PizzaOrderingSystem.Common/GlobalConstants.cs
+++ PizzaOrderingSystem/PizzaOrderingSystem.Common/GlobalConstants.cs
@@ -38,7 +38,8 @@
         public const string LoginAction = "Login";
 
         public const string ShopImageUrl = "/img/PizzaShopLogo.jpg";
-        public const string ShopImageFullUrl = "C:/Users/mitko/Downloads/ASP.NET-Advanced-Final-Project/PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/wwwroot/img/PizzaShopLogo.jpg";
+        public const string ShopImageFullUrlHome = "C:/Users/mitko/Downloads/ASP.NET-Advanced-Final-Project/PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/wwwroot/img/PizzaShopLogo.jpg";
+        public const string ShopImageFullUrlOffice = "C:/Users/PTO/Downloads/ASP.NET-Advanced-Final-Project/PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/wwwroot/img/PizzaShopLogo.jpg";
         public const string MasterCardLogo = "https://img.icons8.com/color/48/000000/mastercard-logo.png";
 
         public const string HomeImageUrl1 = "img/CarouselPhoto1.jpeg";
