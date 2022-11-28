0092a1076b500c66efd6905d7db792b29f9bb6e8
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-29 00:18:37 +0200

    Added new tests

---
+++ PizzaOrderingSystem/Tests/PizzaOrderingSystem.UnitTests/ShopTests.cs
@@ -0,0 +1,94 @@
+﻿
+namespace PizzaOrderingSystem.UnitTests
+{
+    public class ShopTests
+    {
+        private ApplicationDbContext dbContext;
+        private IDeletableEntityRepository<Shop> shopRepo;
+        private DbContextOptionsBuilder<ApplicationDbContext> options;
+        private IShopService shopService;
+        private IEnumerable<Shop> shops;
+        private CreateShopViewModel shop;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "PizzaTestDb");
+            this.dbContext = new ApplicationDbContext(this.options.Options);
+            this.shopRepo = new EfDeletableEntityRepository<Shop>(this.dbContext);
+            this.shopService = new ShopService(shopRepo);
+        }
+
+        [Test]
+        public async Task CreateAsyncAddsCorrect()
+        {
+            var address = new Address()
+            {
+                City = "Sofia",
+                Street = "TestStreet",
+                StreetNumber = 2,
+                PostCode = "1232",
+            };
+
+            shop = new CreateShopViewModel()
+            {
+                Name = "Test",
+                PhoneNumber = "0898898782",
+                Description = "TestDescription",
+                City = address.City,
+                Street = address.Street,
+                StreetNumber= address.StreetNumber,
+            };
+
+            await this.shopService.CreateAsync(shop);
+
+            Assert.That(this.dbContext.Shops.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task GetAllAsyncReturnsCorrectCount()
+        {
+            await this.FillCollection();
+
+            await this.shopService.GetAllAsync();
+
+            Assert.That(this.dbContext.Shops.Count(), Is.EqualTo(4));
+        }
+
+        [Test]
+        public async Task GetAllAsyncReturnsCorrectCountEmpty()
+        {
+            await this.FlushCollection();
+            await this.shopService.GetAllAsync();
+
+            Assert.That(this.dbContext.Shops.Count(), Is.EqualTo(0));
+        }
+
+        private async Task FillCollection()
+        {
+
+            this.shops = new List<Shop>()
+            {
+                new Shop() { Name = "First", Description = "A very short one", PhoneNumber = "0896674396", AddressId = "addressid"},
+                new Shop() { Name = "Second", Description = "A very short one", PhoneNumber = "0896674396", AddressId = "addressid2"},
+                new Shop() { Name = "Third", Description = "A very short one", PhoneNumber = "0896674396",AddressId = "addressid3" },
+            };
+
+            await this.dbContext.AddRangeAsync(this.shops);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        private async Task FlushCollection()
+        {
+            var shops = await this.shopRepo.All().ToListAsync();
+
+            foreach (var shop in shops)
+            {
+                this.dbContext.Shops.Remove(shop);
+            }
+
+            await this.shopRepo.SaveChangesAsync();
+        }
+    }
+}
