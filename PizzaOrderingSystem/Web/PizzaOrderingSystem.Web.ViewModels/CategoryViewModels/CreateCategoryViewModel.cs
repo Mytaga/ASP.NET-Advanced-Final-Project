d9a5e9a3b1abfe506b8dc5973c4e7972944791e7
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-29 10:24:15 +0200

    Delete PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels directory

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/CategoryViewModels/CreateCategoryViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PizzaOrderingSystem.Web.ViewModels.CategoryViewModels
-{
-    public class CreateCategoryViewModel
-    {
-        public string Name { get; set; }
-    }
-}
--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/ReviewViewModels/AllReviewsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace PizzaOrderingSystem.Web.ViewModels.ReviewViewModels
-{
-    public class AllReviewsViewModel
-    {
-        public ICollection<ReviewViewModel> Reviews { get; set; }
-    }
-}
