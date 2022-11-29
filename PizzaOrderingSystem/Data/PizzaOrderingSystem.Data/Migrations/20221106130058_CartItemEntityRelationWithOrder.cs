9e6fc00f2d9a35084e3c3d394196a316e416f02d
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-29 09:58:55 +0200

    Fixed directory

---
+++ PizzaOrderingSystem/Data/PizzaOrderingSystem.Data/Migrations/20221106130058_CartItemEntityRelationWithOrder.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaOrderingSystem.Data.Migrations
+{
+    public partial class CartItemEntityRelationWithOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItemOrder");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderId",
+                table: "CartItems",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_OrderId",
+                table: "CartItems",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Orders_OrderId",
+                table: "CartItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Orders_OrderId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_OrderId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "CartItems");
+
+            migrationBuilder.CreateTable(
+                name: "CartItemOrder",
+                columns: table => new
+                {
+                    OrderProductsId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    OrdersId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItemOrder", x => new { x.OrderProductsId, x.OrdersId });
+                    table.ForeignKey(
+                        name: "FK_CartItemOrder_CartItems_OrderProductsId",
+                        column: x => x.OrderProductsId,
+                        principalTable: "CartItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CartItemOrder_Orders_OrdersId",
+                        column: x => x.OrdersId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItemOrder_OrdersId",
+                table: "CartItemOrder",
+                column: "OrdersId");
+        }
+    }
+}
---
+++ PizzaOrderingSystem/Data/PizzaOrderingSystem.Data/Migrations/20221119141503_ShopEntityFixedRelations.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaOrderingSystem.Data.Migrations
+{
+    public partial class ShopEntityFixedRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Shops_ShopId",
+                table: "Sales");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sales_ShopId",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Sales");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShopId",
+                table: "Sales",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_ShopId",
+                table: "Sales",
+                column: "ShopId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Shops_ShopId",
+                table: "Sales",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id");
+        }
+    }
+}
