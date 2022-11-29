981b6eee2092ecf9f2c1f0d8b8fb7858101c9ad3
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-11-29 16:15:13 +0200

    Fixed comparison between passwords

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/RegisterViewModel.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Account/RegisterViewModel.cs
@@ -9,13 +9,13 @@ namespace PizzaOrderingSystem.Web.ViewModels.Account
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
-        [Compare(nameof(ConfirmPassword))]
+        [Required] 
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
