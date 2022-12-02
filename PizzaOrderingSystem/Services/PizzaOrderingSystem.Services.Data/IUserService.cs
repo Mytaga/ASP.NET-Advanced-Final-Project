364c629a7316190ca800fe63727a88b3fddaac9c
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-12-02 17:10:24 +0200

    Added more functionality

--- PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IUserService.cs
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/IUserService.cs
@@ -1,9 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using PizzaOrderingSystem.Data;
+using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Web.ViewModels.Account;
+using System.Threading.Tasks;
 
 namespace PizzaOrderingSystem.Services.Data
 {
     public interface IUserService
     {
         Task<int> GetUsersCountAsync();
+
+        ProfileViewModel GetUser(ApplicationUser user);
     }
 }
--- PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/UserService.cs
+++ PizzaOrderingSystem/Services/PizzaOrderingSystem.Services.Data/UserService.cs
@@ -2,6 +2,7 @@
 using PizzaOrderingSystem.Common;
 using PizzaOrderingSystem.Data.Common.Repositories;
 using PizzaOrderingSystem.Data.Models;
+using PizzaOrderingSystem.Web.ViewModels.Account;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,29 @@ namespace PizzaOrderingSystem.Services.Data
         public UserService(IDeletableEntityRepository<ApplicationUser> userRepo)
         {
             this.userRepo = userRepo;
+        }
+        public ProfileViewModel GetUser(ApplicationUser user)
+        {
+            var viewModel = new ProfileViewModel()
+            {
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                ImageUrl = user.ImageUrl,
+                PhoneNumber = user.PhoneNumber,
+            };
+
+            if (user.Address != null)
+            {
+                viewModel.City = user.Address.City;
+                viewModel.Street = user.Address.Street;
+                viewModel.StreetNumber = user.Address.StreetNumber;
+                viewModel.Floor = user.Address.Floor;
+                viewModel.PostCode = user.Address.PostCode;
+            }
+
+            return viewModel;
         }
 
         public async Task<int> GetUsersCountAsync()
