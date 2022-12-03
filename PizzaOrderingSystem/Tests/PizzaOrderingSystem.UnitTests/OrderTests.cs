14a6b8ad9a7324cdf5d94a979b2168dacb420702
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-12-04 00:50:36 +0200

    Added new tests

---
+++ PizzaOrderingSystem/Tests/PizzaOrderingSystem.UnitTests/OrderTests.cs
@@ -0,0 +1,31 @@
+﻿namespace PizzaOrderingSystem.UnitTests
+{
+    [TestFixture]
+    public class OrderTests
+    {
+        private ApplicationDbContext dbContext;
+        private IDeletableEntityRepository<Order> orderRepo;
+        private DbContextOptionsBuilder<ApplicationDbContext> options;
+        private IOrderService orderService;
+        private Mock<ICartService> cartServiceMock;
+        private Mock<ISaleService> saleServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.options = new DbContextOptionsBuilder<ApplicationDbContext>()
+              .UseInMemoryDatabase(databaseName: "PizzaTestDb");
+            this.dbContext = new ApplicationDbContext(this.options.Options);
+            this.orderRepo = new EfDeletableEntityRepository<Order>(this.dbContext);
+            this.cartServiceMock = new Mock<ICartService>();
+            this.saleServiceMock = new Mock<ISaleService>();
+            this.orderService = new OrderService(orderRepo, cartServiceMock.Object, saleServiceMock.Object);          
+        }
+
+        [Test]
+        public async Task AddAsyncWorkCorrect()
+        {
+
+        }
+    }
+}
