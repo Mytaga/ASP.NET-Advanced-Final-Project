2b71121c1b2211ef8a4971d1060277f4628eabe2
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-12-08 17:32:17 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Administration/Home/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaOrderingSystem.Web.ViewModels.Administration.Home
+{
+    public class IndexViewModel
+    {
+        public int RegisteredUsers { get; set; }
+
+        public int AvailableProducts { get; set; }
+
+        public int OrdersMade { get; set; }
+
+        public int ReviewsPublished { get; set; }
+    }
+}
---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Administration/Home/RegisteredUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Web.ViewModels.Administration.Home
+{
+    public class RegisteredUserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public int OrdersMade { get; set; }
+    }
+}
