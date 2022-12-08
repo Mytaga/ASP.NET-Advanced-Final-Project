d7a36c1fed43f38d89724169601967c83e70311e
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-12-08 15:40:47 +0200

    Added again all unit tests

---
+++ PizzaOrderingSystem/Tests/PizzaOrderingSystem.Web.Tests/PizzaOrderingSystem.UnitTests/ReviewTests.cs
@@ -0,0 +1,141 @@
+﻿using PizzaOrderingSystem.Web.ViewModels.CategoryViewModels;
+
+namespace PizzaOrderingSystem.UnitTests
+{
+    public class ReviewTests
+    {
+        private ApplicationDbContext dbContext;
+        private IDeletableEntityRepository<Review> reviewRepo;
+        private DbContextOptionsBuilder<ApplicationDbContext> options;
+        private IReviewService reviewService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "PizzaTestDb");
+            this.dbContext = new ApplicationDbContext(this.options.Options);
+            this.reviewRepo = new EfDeletableEntityRepository<Review>(this.dbContext);
+            this.reviewService = new ReviewService(reviewRepo);
+        }
+
+        [Test]
+        public async Task AddReviewAddsCorrect()
+        {
+            var model = await this.CreateModel();
+            await this.reviewService.AddReview(model, model.UserId);
+
+            Assert.That(this.reviewRepo.All().Count(), Is.EqualTo(1));  
+        }
+
+        [Test]
+        public async Task AddReviewAddsCorrectReview()
+        {
+            var model = await this.CreateModel();
+            await this.reviewService.AddReview(model, model.UserId);
+
+            Assert.That(this.reviewRepo.All().Any(r => r.AuthorName == "Mitko"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public async Task GetAllAsyncReturnsCorrect()
+        {
+            var reviews = await this.reviewService.GetAllAsync();
+
+            Assert.That(reviews.Reviews.Count(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public async Task GetAllAsyncReturnsCorrectWithEmptyCollection()
+        {
+            await this.FlushCollection();
+
+            var reviews = await this.reviewService.GetAllAsync();
+
+            Assert.That(reviews.Reviews.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task GetAllReviewsCountAsyncReturnsCorrectWithEmptyCollection()
+        {
+            await this.FlushCollection();
+
+            int count = await this.reviewService.GetAllReviewsCountAsync();
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task GetAllReviewsCountAsyncReturnsCorrect()
+        {
+            var model = await this.CreateModel();
+
+            await this.reviewService.AddReview(model, model.UserId);
+
+            int count = await this.reviewService.GetAllReviewsCountAsync();
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task GetByIdReturnsCorrectReview()
+        {
+            var createModel = await this.CreateModel();
+
+            await this.reviewService.AddReview(createModel, createModel.UserId);
+
+            var model = await this.reviewRepo.AllAsNoTracking().FirstOrDefaultAsync(r => r.AuthorName == "Mitko");
+
+            var testModel = await this.reviewService.GetByIdAsync(model.Id);
+
+            Assert.That(testModel, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task DeleteReviewWorksCorrect()
+        {
+            var createModel = await this.CreateModel();
+
+            await this.reviewService.AddReview(createModel, createModel.UserId);
+
+            var review = await this.reviewRepo.AllAsNoTracking().FirstOrDefaultAsync(r => r.AuthorName == "Mitko");
+
+            await this.reviewService.DeleteReview(review);
+
+            Assert.That(this.reviewRepo.All().Contains(review), Is.False);
+        }
+
+        private async Task<CreateReviewInputModel> CreateModel()
+        {
+            var user = new ApplicationUser()
+            {
+                FirstName = "Mitko",
+                LastName = "Kralev",
+            };
+
+            await this.dbContext.Users.AddAsync(user);
+            await this.dbContext.SaveChangesAsync();
+
+            var model = new CreateReviewInputModel()
+            {
+                AuthorName = user.FirstName, 
+                Content = "Bla bla bla such a dumb comment!",              
+                UserId = user.Id,
+            };
+
+            return model;
+        }
+
+        public async Task FlushCollection()
+        {
+            var reviews = await this.reviewRepo.All().ToListAsync();
+
+            foreach (var review in reviews)
+            {
+                this.dbContext.Reviews.Remove(review);
+            }
+
+            await this.reviewRepo.SaveChangesAsync();
+        }
+    }
+}
---
+++ PizzaOrderingSystem/Tests/PizzaOrderingSystem.Web.Tests/PizzaOrderingSystem.UnitTests/UserTests.cs
@@ -0,0 +1,177 @@
+﻿using PizzaOrderingSystem.Data.Models;
+
+namespace PizzaOrderingSystem.UnitTests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        private ApplicationDbContext dbContext;
+        private IDeletableEntityRepository<ApplicationUser> userRepo;
+        private DbContextOptionsBuilder<ApplicationDbContext> options;
+        private IUserService userService;
+        private IEnumerable<ApplicationUser> users;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "PizzaTestDb");
+            this.dbContext = new ApplicationDbContext(this.options.Options);
+            this.userRepo = new EfDeletableEntityRepository<ApplicationUser>(this.dbContext);
+            this.userService = new UserService(userRepo);
+        }
+
+        [Test]
+        public async Task GetUsersCountAsyncReturnsCorrect()
+        {
+            await this.FlushCollection();
+
+            await this.FillCollection();
+
+            var usersCount = await this.userService.GetUsersCountAsync();
+
+            Assert.That(usersCount, Is.EqualTo(3));
+        }
+
+        [Test]
+        public async Task GetUsersCountAsyncReturnsCorrectWithEmptyCollection()
+        {
+            await this.FlushCollection();
+
+            var usersCount = await this.userService.GetUsersCountAsync();
+
+            Assert.That(usersCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetUserReturnsCorrectInfo()
+        {
+            var address = new Address()
+            {
+                City = "Sofia",
+                Street = "Street",
+                StreetNumber = 1,
+                Floor = 1,
+                PostCode = "1231",
+            };
+
+            var appUser = new ApplicationUser()
+            {
+                FirstName = "Hristo",
+                LastName = "Stoichkov",
+                Address = address,
+            };
+
+            var user = this.userService.GetUser(appUser);
+
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user.FirstName, Is.EqualTo(appUser.FirstName));
+            Assert.That(user.LastName, Is.EqualTo(appUser.LastName));
+            Assert.That(user.Email, Is.EqualTo(appUser.Email));
+            Assert.That(user.PhoneNumber, Is.EqualTo(appUser.PhoneNumber));
+            Assert.That(user.City, Is.EqualTo(appUser.Address.City));
+            Assert.That(user.Street, Is.EqualTo(appUser.Address.Street));
+            Assert.That(user.StreetNumber, Is.EqualTo(appUser.Address.StreetNumber));
+            Assert.That(user.Floor, Is.EqualTo(appUser.Address.Floor));
+            Assert.That(user.PostCode, Is.EqualTo(appUser.Address.PostCode));
+        }
+
+        [Test]
+        public void GetUserReturnsCorrectInfoWithoutAddress()
+        {
+            var appUser = new ApplicationUser()
+            {
+                FirstName = "Hristo",
+                LastName = "Stoichkov",
+            };
+
+            var user = this.userService.GetUser(appUser);
+
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user.FirstName, Is.EqualTo(appUser.FirstName));
+            Assert.That(user.LastName, Is.EqualTo(appUser.LastName));
+            Assert.That(user.Email, Is.EqualTo(appUser.Email));
+            Assert.That(user.PhoneNumber, Is.EqualTo(appUser.PhoneNumber));
+            Assert.That(user.City, Is.Null);
+            Assert.That(user.Street, Is.Null);
+            Assert.That(user.StreetNumber, Is.EqualTo(0));
+            Assert.That(user.Floor, Is.EqualTo(0));
+            Assert.That(user.PostCode, Is.Null);
+        }
+
+        [Test]
+        public void GetProfileViewModelReturnsCorrect()
+        {
+            var address = new Address()
+            {
+                City = "Sofia",
+                Street = "Street",
+                StreetNumber = 1,
+                Floor = 1,
+                PostCode = "1231",
+            };
+
+            var appUser = new ApplicationUser()
+            {
+                FirstName = "Hristo",
+                LastName = "Stoichkov",
+                PhoneNumber = "0898231282",
+                Address = address,
+            };
+
+            var user = this.userService.GetUpdateProfileView(appUser);
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user.PhoneNumber, Is.EqualTo(appUser.PhoneNumber));
+            Assert.That(user.City, Is.EqualTo(appUser.Address.City));
+            Assert.That(user.Street, Is.EqualTo(appUser.Address.Street));
+            Assert.That(user.StreetNumber, Is.EqualTo(appUser.Address.StreetNumber));
+            Assert.That(user.Floor, Is.EqualTo(appUser.Address.Floor));
+            Assert.That(user.PostCode, Is.EqualTo(appUser.Address.PostCode));
+        }
+
+        [Test]
+        public async Task GetAllRegisteredUsersAsyncReturnsCorrect()
+        {
+            await this.FlushCollection();
+            await this.FillCollection();
+
+            var users = await this.userService.GetAllRegisterdUsersAsync();
+
+            Assert.That(users.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public async Task GetAllRegisteredUsersAsyncReturnsCorrectWithEmptyCollection()
+        {
+            await this.FlushCollection();
+
+            var users = await this.userService.GetAllRegisterdUsersAsync();
+
+            Assert.That(users.Count, Is.EqualTo(0));
+        }
+
+        private async Task FillCollection()
+        {
+            this.users = new List<ApplicationUser>()
+            {
+                new ApplicationUser(){ FirstName = "First", LastName = "Last"},
+                new ApplicationUser(){ FirstName = "Second", LastName = "LastSecond"},
+                new ApplicationUser(){ FirstName = "Third", LastName = "LastThird"},
+            };
+
+            this.dbContext.Users.AddRange(users);
+            await this.dbContext.SaveChangesAsync();
+        }
+        private async Task FlushCollection()
+        {
+            var users = await this.userRepo.All().ToListAsync();
+
+            foreach (var user in users)
+            {
+                this.dbContext.Users.Remove(user);
+            }
+
+            await this.userRepo.SaveChangesAsync();
+        }
+    }
+}
