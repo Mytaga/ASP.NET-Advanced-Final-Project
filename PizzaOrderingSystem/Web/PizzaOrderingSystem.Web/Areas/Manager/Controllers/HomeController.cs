0e2547294dd9422b128e193c7d28a66251d3e1e0
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-12-10 16:07:25 +0200

    Exception handling added

--- PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Areas/Manager/Controllers/HomeController.cs
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web/Areas/Manager/Controllers/HomeController.cs
@@ -1,23 +1,38 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PizzaOrderingSystem.Common;
 using PizzaOrderingSystem.Web.Areas.Administration.Controllers;
 using PizzaOrderingSystem.Web.ViewModels;
+using PizzaOrderingSystem.Web.ViewModels.Manager.Home;
 using System.Diagnostics;
 
 namespace PizzaOrderingSystem.Web.Areas.Manager.Controllers
 {
     public class HomeController : ManagerController
     {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var viewModel = new IndexViewModel();
+            return this.View(viewModel);
         }
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var feature = this.HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            this.logger.LogError(feature.Error, "TraceIdentifier: {0}", HttpContext.TraceIdentifier);
+
             return this.View(
                 new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
