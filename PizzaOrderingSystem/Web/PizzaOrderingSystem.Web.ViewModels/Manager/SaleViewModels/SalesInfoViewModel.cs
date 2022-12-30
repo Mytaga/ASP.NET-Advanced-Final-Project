93faedca3ed122bcbd27e285c1dfc4f8811c3ce1
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-12-30 18:02:01 +0200

    New model

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Manager/SaleViewModels/SalesInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Web.ViewModels.Manager.SaleViewModels
+{
+    public class SalesInfoViewModel
+    {
+        public string TotalAmount { get; set; }
+
+        public string TotalCount { get; set; }
+    }
+}
