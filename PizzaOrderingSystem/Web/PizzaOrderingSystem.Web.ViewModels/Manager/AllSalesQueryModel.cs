7afaa9937082e87752662ebafb44a8a43a844bd9
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2023-01-31 00:00:15 +0200

    Add files via upload

---
+++ PizzaOrderingSystem/Web/PizzaOrderingSystem.Web.ViewModels/Manager/AllSalesQueryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingSystem.Web.ViewModels.Manager.SaleViewModels
+{
+    public class AllSalesQueryModel
+    {
+        public const int SalesPerPage = 5;
+
+        public int CurrentPage { get; set; } = 1;
+
+        public int TotalSalesCount { get; set; }
+        
+        public IEnumerable<SaleViewModel> Sales { get; set; } = new List<SaleViewModel>();
+    }
+}
